eaf29730c0b7c465a30381d3faae0398be58b7d0
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-05 20:18:59 -0800

    Got user data

--- frontend/pages/api/profiles/me.tsx
+++ frontend/pages/api/profiles/me.tsx
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from "next";
-import {COOKIES, getSupabaseUser} from "../../../lib/cookies"
-import supabaseAdmin from "../../../lib/supabaseAdmin"
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const user = await getSupabaseUser(req.cookies[COOKIES.access_token], req.cookies[COOKIES.refresh_token])
-    if(!user){
-        return res.status(401).json({ error: 'User not found' });
-    }
-    return res.status(200).json(user)
+import { COOKIES, getSupabaseUser } from "../../../lib/cookies";
+import supabaseAdmin from "../../../lib/supabaseAdmin";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const user = await getSupabaseUser(
+    req.cookies[COOKIES.access_token],
+    req.cookies[COOKIES.refresh_token]
+  );
+  if (!user) {
+    return res.status(401).json({ error: "User not found" });
   }
-  
+  const { data, error } = await supabaseAdmin
+    .from("profiles")
+    .select("*")
+    .eq("user_id", user.data.user.id);
+  if (error) {
+    return res.status(400).json({ error: error });
+  }
+  return res.status(200).json(data);
+}
