c932adb07359cf089865859990f6c139c526450a
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-06 13:37:51 -0800

    Update create-event.jsx

--- frontend/pages/create-event.jsx
+++ frontend/pages/create-event.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Wrapper from '../layout/wrapper';
 import SEO from '../components/seo';
 import CreateEventMain from '../components/createEventPage';
+import { mustBeLoggedInServer } from '../lib/authorization';
 
 const index = () => {
     return (
@@ -11,5 +12,9 @@ const index = () => {
         </Wrapper>
     );
 };
+
+export const getServerSideProps = async (context) => {
+    return mustBeLoggedInServer(context);
+  };
 
 export default index;
