3b53fa101095ed6bed1dfac49783b834f529c0f3
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-22 11:30:48 -0800

    Fixed bar flickering

--- frontend/components/common/breadcamMenu.jsx
+++ frontend/components/common/breadcamMenu.jsx
@@ -1,25 +1,18 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const BreadcamMenu = ({title}) => {
-    return (
-      <div className="breadcrumb__menu">
+const BreadcamMenu = ({ title }) => {
+  return (
+    <div className="breadcrumb__menu">
       <nav>
         <ul>
-          <li>
-            <span>
-              <Link legacyBehavior href="/dashboard">
-                <a> Home </a>
-              </Link>
-            </span>
-          </li>
           <li className="active">
             <span> {title} </span>
           </li>
         </ul>
       </nav>
     </div>
-    );
+  );
 };
 
 export default BreadcamMenu;
