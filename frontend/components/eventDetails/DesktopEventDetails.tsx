7fc609ce2d94980131db99c0b0a9ae5ee23209d7
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-23 11:58:54 -0800

    Added animated background to event details

--- frontend/components/eventDetails/DesktopEventDetails.tsx
+++ frontend/components/eventDetails/DesktopEventDetails.tsx
@@ -4,6 +4,7 @@ import { COLORS } from "../../constants/colors";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LocationOnOutlined, AccessTimeOutlined } from "@mui/icons-material";
 import Footer from "../landingPage/component/footer";
+import styles from "./styles.module.scss";
 
 const DesktopEventDetails = ({ event, host }) => {
   function formatDateString(dateString) {
@@ -54,60 +55,70 @@ const DesktopEventDetails = ({ event, host }) => {
   }
 
   return (
-    <div style={{ paddingLeft: 60, paddingRight: 60, marginTop: 150 }}>
-      <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
-        {/* Event information on the left */}
-        <div style={{ width: "50%", paddingRight: "20px" }}>
-          {/* Add some spacing between the columns */}
-          <h4
-            style={{
-              color: COLORS.secondaryText,
-              fontSize: 30,
-              fontWeight: 200,
-              marginBottom: 10,
-              marginRight: 30,
-            }}
-          >
-            {host.name}
-          </h4>
-          <h1
-            style={{
-              color: COLORS.mainText,
-              fontSize: 80,
-              fontWeight: 700,
-              marginBottom: 15,
-            }}
-          >
-            {event.title}
-          </h1>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "row",
-              marginTop: 20,
-            }}
-          >
-            <LocationOnOutlined
-              style={{ color: COLORS.purple, fontSize: 30, marginRight: 10 }}
-            />
-            <h3
-              style={{ color: COLORS.mainText, fontSize: 20, fontWeight: 200 }}
+      <div style={{ paddingLeft: 60, paddingRight: 60, paddingTop: 150, position: 'relative' }}>
+        <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
+          {/* Event information on the left */}
+          <div style={{ width: "50%", paddingRight: "20px" }}>
+            {/* Add some spacing between the columns */}
+            <h4
+              style={{
+                color: COLORS.secondaryText,
+                fontSize: 30,
+                fontWeight: 200,
+                marginBottom: 10,
+                marginRight: 30,
+              }}
             >
-              {event.location}
-            </h3>
-          </div>
-          <div style={{ display: "flex", flexDirection: "row", marginTop: 20 }}>
-            <AccessTimeOutlined
-              style={{ color: COLORS.purple, fontSize: 30, marginRight: 10 }}
-            />
-            <h3
-              style={{ color: COLORS.mainText, fontSize: 20, fontWeight: 200 }}
+              {host.name}
+            </h4>
+            <h1
+              style={{
+                color: COLORS.mainText,
+                fontSize: 80,
+                fontWeight: 700,
+                marginBottom: 15,
+              }}
             >
-              {formatDateString(event.start_date)}
-            </h3>
-          </div>
-          <div
+              {event.title}
+            </h1>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                marginTop: 20,
+              }}
+            >
+              <LocationOnOutlined
+                style={{ color: COLORS.purple, fontSize: 30, marginRight: 10 }}
+              />
+              <h3
+                style={{
+                  color: COLORS.mainText,
+                  fontSize: 20,
+                  fontWeight: 200,
+                }}
+              >
+                {event.location}
+              </h3>
+            </div>
+            <div
+              style={{ display: "flex", flexDirection: "row", marginTop: 20 }}
+            >
+              <AccessTimeOutlined
+                style={{ color: COLORS.purple, fontSize: 30, marginRight: 10 }}
+              />
+              <h3
+                style={{
+                  color: COLORS.mainText,
+                  fontSize: 20,
+                  fontWeight: 200,
+                }}
+              >
+                {formatDateString(event.start_date)}
+              </h3>
+            </div>
+            <div
               style={{
                 display: "flex",
                 justifyContent: "center", // Center horizontally
@@ -123,6 +134,7 @@ const DesktopEventDetails = ({ event, host }) => {
                   fontSize: "20px", // Adjust font size to your preference
                   fontWeight: "600", // Adjust font weight as needed
                   border: "none",
+                  fontFamily: "Poppins",
                   borderRadius: "10px", // Adjust border radius to match the button in the image
                   cursor: "pointer",
                   outline: "none", // Remove outline on focus for cleaner design
@@ -134,65 +146,66 @@ const DesktopEventDetails = ({ event, host }) => {
                 Signup for Event
               </button>
             </div>
-        </div>
+          </div>
 
-        {/* Image on the right */}
-        <div
+          {/* Image on the right */}
+          <div
+            style={{
+              width: "50%",
+              flexDirection: "column",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={event.image}
+              style={{
+                maxWidth: "100%", // Limits the width to the parent container's width
+                height: "auto", // Maintains the aspect ratio
+                maxHeight: "60vh",
+                objectFit: "contain", // Ensures the entire image is visible
+                borderRadius: "10px",
+                minHeight: "500px", // Set minimum height dynamically
+              }}
+              alt={event.title}
+            />
+          </div>
+        </div>
+        <hr
           style={{
-            width: "50%",
-            flexDirection: "column",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            width: "100%",
+            border: ".5px solid #E0E0E0",
+            marginTop: "50px",
+          }}
+        />
+        <h3
+          style={{
+            color: COLORS.mainText,
+            fontSize: 30,
+            fontWeight: 200,
+            marginBottom: 30,
           }}
         >
-          <img
-            src={event.image}
-            style={{
-              maxWidth: "100%", // Limits the width to the parent container's width
-              height: "auto", // Maintains the aspect ratio
-              maxHeight: "60vh",
-              objectFit: "contain", // Ensures the entire image is visible
-              borderRadius: "10px",
-              minHeight: '500px', // Set minimum height dynamically
-            }}
-            alt={event.title}
-          />
-        </div>
-      </div>
-      <hr
-        style={{
-          width: "100%",
-          border: ".5px solid #E0E0E0",
-          marginTop: "50px",
-        }}
-      />
-      <h3
-        style={{
-          color: COLORS.mainText,
-          fontSize: 30,
-          fontWeight: 200,
-          marginBottom: 30,
-        }}
-      >
-        About this event
-      </h3>
-      <h3
-        style={{ color: COLORS.secondaryText, fontSize: 20, fontWeight: 400 }}
-      >
-        {event.description}
-      </h3>
-      <hr
-        style={{
-          width: "100%",
-          border: ".5px solid #E0E0E0",
-          marginTop: "30px",
-          marginBottom: "30px"
-        }}
-      />
+          About this event
+        </h3>
+        <h3
+          style={{ color: COLORS.secondaryText, fontSize: 20, fontWeight: 400 }}
+        >
+          {event.description}
+        </h3>
+        <hr
+          style={{
+            width: "100%",
+            border: ".5px solid #E0E0E0",
+            marginTop: "30px",
+            marginBottom: "30px",
+          }}
+        />
 
-      <Footer />
-    </div>
+        <Footer />
+        <div className={styles.eventDetailsBackground}/>
+      </div>
   );
 };
 
