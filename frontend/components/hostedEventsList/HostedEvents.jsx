53e2d16f13ca1bd39519655beba2e9cfbf31a00a
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-23 21:16:36 -0800

    Added hosted events dashboard

--- frontend/components/hostedEventsList/HostedEvents.jsx
+++ frontend/components/hostedEventsList/HostedEvents.jsx
@@ -6,6 +6,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useInView } from "react-intersection-observer";
 import showMessage from "../errorMessage/showMessage";
 import index from "../../pages/dashboard";
+import { COLORS } from "../../constants/colors";
 
 const HostedEvents = ({ user }) => {
   const [events, setEvents] = useState([]);
@@ -17,6 +18,37 @@ const HostedEvents = ({ user }) => {
   const { ref, inView } = useInView({
     threshold: 0,
   });
+
+  function formatISODateString(isoString) {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const date = new Date(isoString);
+
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+
+    return `${month} ${day}, ${year} @ ${hours}:${formattedMinutes}${ampm}`;
+  }
 
   const fetchEvents = async () => {
     if (!hasMoreRef.current || loadingRef.current) return;
@@ -35,8 +67,8 @@ const HostedEvents = ({ user }) => {
         hasMoreRef.current = false;
       } else {
         setEvents((prevEvents) => [...prevEvents, ...newEvents]);
-        page.current= page.current + 1;
-        console.log(page.current)
+        page.current = page.current + 1;
+        console.log(page.current);
       }
     } catch (e) {
       showMessage("Error pulling events: " + e.message, true);
@@ -118,16 +150,71 @@ const HostedEvents = ({ user }) => {
           </div>
         </div>
 
-        {events.map((event, index) => {
-          return (
-            <div>
-              <div style={{ height: "70vh" }}>
-                {index + 1}. {event.title}
+        <div className="row">
+          {events.map((event, index) => {
+            const eventUrl = `/event/${event.event_id}`;
+            const signups = 20;
+            return (
+              <div className="col-xxl-4 col-xl-6 col-12">
+                <a href={eventUrl} style={{ textDecoration: "none" }}>
+                  <div
+                    style={{
+                      backgroundColor: "#2D2D2D",
+                      margin: 10,
+                      paddingLeft: 20,
+                      paddingRight: 20,
+                      paddingTop: 10,
+                      paddingBottom: 10,
+                      borderRadius: 10,
+                      border: ".5px solid #3D3D3D",
+                      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' // Adds shadow
+                    }}
+                  >
+                    <h1
+                      style={{
+                        fontWeight: 600,
+                        fontSize: 30,
+                        color: COLORS.mainText,
+                        marginBottom: 5,
+                      }}
+                    >
+                      {event.title}
+                    </h1>
+                    <h3
+                      style={{
+                        fontWeight: 400,
+                        fontSize: 16,
+                        color: COLORS.secondaryText,
+                      }}
+                    >
+                      {formatISODateString(event.start_date)}
+                    </h3>
+                    <h3
+                      style={{
+                        fontWeight: 400,
+                        fontSize: 16,
+                        color: COLORS.secondaryText,
+                      }}
+                    >
+                      {signups} signups
+                    </h3>
+                    <img
+                      src={event.image}
+                      style={{
+                        marginTop: 15,
+                        borderRadius: 10,
+                        height: "300px",
+                        width: "100%", // Ensures the image covers the entire width of the container
+                        objectFit: "cover", // Makes the image cover the entire area without stretching
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)' // Adds shadow
+                      }}
+                    />
+                  </div>
+                </a>
               </div>
-              <div>{event.start_date}</div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
         <div ref={ref} style={{ height: "20px" }}></div>
       </div>
     </>
