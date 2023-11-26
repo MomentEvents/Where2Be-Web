875f8827a48f3d13af08644f5ed0ee683606d78e
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-26 01:11:32 -0800

    Removed unused components for vercel compilation

--- frontend/components/landingPage/index.jsx
+++ frontend/components/landingPage/index.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import LandingHeader from "./component/header";
 import LandingFooter from "./component/footer";
 import Features from "./component/features";
-import MainAria from "./component/mainArea";
-import BackToTopCom from "../common/scroll-to-top";
 import LandingSidemenu from "../common/sidemenu-landing/landingSidemenu";
-import DarkMode from "../common/dark-mode";
 import { useContext } from "react";
 import { DarkModeContext } from "../darkmode-provider/DarkModeProvider";
-import background from "../../public/assets/img/index/background.png";
 import CirclingLightsBackground from "../Styles/CirclingLightsBackground";
 
 const LandingPageMain = () => {
