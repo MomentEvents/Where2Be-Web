a56d2bf5db1fc567c2108e07aff3fae2981990a3
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2024-01-08 00:14:04 -0800

    Moved speed and analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -9,24 +9,24 @@ import SEO from "../components/seo";
 import { DarkModeProvider } from "../components/darkmode-provider/DarkModeProvider";
 import supabase from "../lib/supabase";
 import { deleteSupabaseCookies, setSupabaseCookies } from "../lib/cookies";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import '../components/attendeeEventDetails/PhoneNumber.css'
-import { Analytics } from '@vercel/analytics/react';
+import "../components/attendeeEventDetails/PhoneNumber.css";
+import { Analytics } from "@vercel/analytics/react";
 
-import NProgress from 'nprogress';
-import Router from 'next/router';
+import NProgress from "nprogress";
+import Router from "next/router";
 
-import "../public/assets/app/css/globals.css"; 
+import "../public/assets/app/css/globals.css";
 
 // Optional: import nprogress.css and customize it as needed
-import 'nprogress/nprogress.css';
+import "nprogress/nprogress.css";
 
 NProgress.configure({ showSpinner: false });
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   supabase.auth.onAuthStateChange((event, session) => {
@@ -41,16 +41,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <SEO
-        font={"https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"} pageTitle={undefined} pageImage={undefined} pageDescription={undefined}      />
+        font={
+          "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        }
+        pageTitle={undefined}
+        pageImage={undefined}
+        pageDescription={undefined}
+      />
       <Provider store={store}>
         <DarkModeProvider>
           <AppProvider>
             <Component {...pageProps} />
+            <SpeedInsights />
+            <Analytics />
           </AppProvider>
         </DarkModeProvider>
       </Provider>
-      <SpeedInsights/>
-      <Analytics/>
     </>
   );
 }
